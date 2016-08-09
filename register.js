8197e253c130e00f6e5841d7371bcfedaa6c8257
Michael Needleman <mcn46@drexel.edu>
2016-08-09 15:06:48 -0400

    Validate.js, slight updates to js files

--- register.js
+++ register.js
@@ -3,11 +3,39 @@ $(document).ready(function()
 	$(".submit").click(function()
 	{
 		addUser(wrapUser());
+		validateEmail();
 	});
 });
 function wrapUser()
 {
+	var emailU=$(".emailField")[0].value;
+	var usernameU=$(".usernameField")[0].value;
+	var pwU=$(".pwField")[0].value;
+	var pwU2=$(".pw2Field")[0].value;
+	var TFAU=$(".2FA")[0].value;
+	
+	if(pwU!=pwU2)
+	{
+		console.error("Error, passwords do not match");
+		return null;
+	}
+
+	var user=
+	{
+		email: emailU,
+		username: usernameU,
+		pw: pwU,
+		TFA: TFAU
+	}
+	return user;
 }
-function addUser()
+function addUser(user)
 {
+	console.log(user);
+}
+function validateEmail(email)
+{
+	var reg=/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/;
+
+	console.log(reg);
 }
---
+++ validate.js
@@ -0,0 +1,12 @@
+function validateEmail(email)
+{
+}
+function validatePWS(pw1,pw2)
+{
+}
+function validateUsername(username)
+{
+}
+function validate2FA(TFA)
+{
+}
