01873012d79e16a03f5f32941ef0ed44ad8d206c
Amir Omidi <amir@aaomidi.com>
2016-08-11 16:00:51 -0400

    Dynamic navbar!

---
+++ templates/js/load.js
@@ -0,0 +1,36 @@
+var load = function (doc, site, func) {
+    'use strict';
+
+    if (!window.XMLHttpRequest && 'ActiveXObject' in window) {
+        window.XMLHttpRequest = function () {
+            return new ActiveXObject('MSXML2.XMLHttp');
+        };
+    }
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open("GET", "/templates/html/navbar.html", true);
+
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState !== 4 || this.status !== 200) {
+            document.getElementById('navbar').innerHTML = "<b> Error reading navbar.html </b>";
+            return;
+        }
+
+        document.getElementById('navbar').innerHTML = this.responseText;
+        //setHighlight(site);
+        if (func != null && typeof  func === "function") {
+            func();
+        }
+    };
+
+    xmlhttp.send();
+
+    return xmlhttp.responseText;
+};
+
+function setHighlight(site) {
+    var id = site.toLowerCase();
+    id = "#nav-" + id;
+    $(id).addClass("active");
+}
