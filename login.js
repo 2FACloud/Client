100f04877a437ab022070deac9ee75f4f955c940
Amir Omidi <amir@aaomidi.com>
2016-08-12 16:44:26 -0400

    Cleaned up the Login code, removed the login.html page since we could just use homepage as it.

--- login.js
+++ login.js
@@ -1,12 +1,26 @@
-function formToJSON(user, pw)
-{
-	var json=
-	{user:user,pw:md5(pw)};
-	console.log(json);
+function formToJSON(user, pw) {
+    var json = {
+        user: user,
+        pw: md5(pw)
+    };
+    console.log(json);
 }
-function doThis()
-{
-	var user=$(".usernameField")[0].value;
-	var pw=$(".pwField")[0].value;
-	formToJSON(user,pw);
+function doThis() {
+    var user = $(".usernameField")[0].value;
+    var pw = $(".pwField")[0].value;
+    formToJSON(user, pw);
+}
+function callLogin() {
+    var username = $("#usernameField").val();
+    var password = $("#passwordField").val();
+    var hashedPassword = md5(password);
+    var userIP = '127.0.0.1'; // TODO get the IP using NodeJS
+
+    var jsonObj = {
+        username: username,
+        password: hashedPassword,
+        userIP: userIP
+    };
+
+    console.log(JSON.stringify(jsonObj, null, 2));
 }
